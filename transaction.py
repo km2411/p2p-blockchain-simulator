a0c4d0223f2efb73a1616190bae04f0f51c892b4
Shubha Gupta <shubha1593@gmail.com>
2018-02-10 10:31:45 +0000

    Get current time using time module.

--- transaction.py
+++ transaction.py
@@ -1,3 +1,5 @@
+import time
+
 class transaction:
 
     def __init__(self, sender,receiver, coins):
@@ -5,7 +7,7 @@ class transaction:
         self.sender = sender
         self.receiver = receiver
         self.coins = coins
-        self.timestamp = #currenttime
+        self.timestamp = time.time()
 #
     def __repr__(self):
         #TxnID: ID x pays ID y C coins
