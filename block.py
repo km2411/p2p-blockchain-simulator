c52d049015d11412b5c207d31a5a6682ae4b1012
Shubha Gupta <shubha1593@gmail.com>
2018-02-10 10:37:03 +0000

    Add time.

--- block.py
+++ block.py
@@ -1,11 +1,13 @@
+import time
+
 class block:
 
-		size = 5 # number of transactions
+	size = 5 # number of transactions
 
     def __init__(self, listofTransactions):
         self.blkid = #increment a global txid plus current time
         self.transactions = listofTransactions
-        self.timestamp = #currenttime
+        self.timestamp = time.time()
 #
     def __repr__(self):
         return str(self.blkid)
